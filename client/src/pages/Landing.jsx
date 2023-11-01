a81356123c21065194def9ca097204af246b325c
nikhil <nikhilmahato3337@gmail.com>
2023-11-01 11:53:24 +0530

    Landing page setup

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import styled from "styled-components";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Wrapper>
+      <h1>Landing Page </h1>
+      <div className="content"> some content </div>
+    </Wrapper>
+  );
 };
 
+const Wrapper = styled.div`
+  background: red;
+  h1 {
+    color: white;
+  }
+  .content {
+    background: blue;
+    color: yellow;
+  }
+`;
 export default Landing;
