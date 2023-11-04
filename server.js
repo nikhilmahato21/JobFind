0f37606a4796a1a4aa34ebf399f0741c734a4637
nikhil <nikhilmahato3337@gmail.com>
2023-11-04 15:21:19 +0530

    get single job

--- server.js
+++ server.js
@@ -37,6 +37,16 @@ app.post("/api/v1/jobs", (req, res) => {
   jobs.push(job);
   res.status(200).json({ job });
 });
+// GET SINGLE JOB
+app.get("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+
+  if (!job) {
+    return res.status(404).json({ msg: `No job with Id ${id}` });
+  }
+  res.status(200).json({ job });
+});
 
 const port = process.env.PORT || 5100;
 
