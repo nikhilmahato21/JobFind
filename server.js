7d8cd1cdf87836755e958ff4dd5af7bbd3538f87
nikhil <nikhilmahato3337@gmail.com>
2023-11-05 21:52:45 +0530

    delete job

--- server.js
+++ server.js
@@ -64,6 +64,20 @@ app.patch("/api/v1/jobs/:id", (req, res) => {
   res.status(200).json({ msg: "job modified", job });
 });
 
+//DELETE JOB
+
+app.delete("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+
+  res.status(200).json({ msg: "job deleted" });
+});
+
 const port = process.env.PORT || 5100;
 
 app.listen(port, () => console.log(`server is running on PORT ${port}`));
