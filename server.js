e2d5a3b7a5b4a23802b46fb6a168155372e36504
nikhil <nikhilmahato3337@gmail.com>
2023-11-05 22:05:01 +0530

    not found and error route

--- server.js
+++ server.js
@@ -78,6 +78,15 @@ app.delete("/api/v1/jobs/:id", (req, res) => {
   res.status(200).json({ msg: "job deleted" });
 });
 
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: "something went wrong" });
+});
+
 const port = process.env.PORT || 5100;
 
 app.listen(port, () => console.log(`server is running on PORT ${port}`));
