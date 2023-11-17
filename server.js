3a76fc2d3f74c17fb9ac8114d147a17ff78f2370
nikhil <nikhilmahato3337@gmail.com>
2023-11-17 20:28:29 +0530

    express validator testing

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 import jobRouter from "./routes/jobRouter.js";
-
 import mongoose from "mongoose";
-
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { body, validationResult } from "express-validator";
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -21,9 +20,24 @@ app.get("/", (req, res) => {
   res.send("home page");
 });
 
-app.post("/", (req, res) => {
-  res.json({ message: "data recieved", data: req.body });
-});
+app.post(
+  "/api/v1/test",
+  [
+    body("name").notEmpty().withMessage("name is required"),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        const errorMessages = errors.array().map((err) => err.msg);
+        return res.status(400).json({ errors: errorMessages });
+      }
+      next();
+    },
+  ],
+  (req, res) => {
+    const { name } = req.body;
+    res.json({ message: `hello ${name}` });
+  }
+);
 
 app.use("/api/v1/jobs", jobRouter);
 
