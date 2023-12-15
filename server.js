81f6f78bbe205b00f146a1a91c3720ac08861a9d
nikhil <nikhilmahato3337@gmail.com>
2023-12-15 14:51:11 +0530

    user routes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import cookieParser from "cookie-parser";
 
 import jobRouter from "./routes/jobRouter.js";
 import authRouter from "./routes/authRouter.js";
+import userRouter from "./routes/userRouter.js";
 
 import mongoose from "mongoose";
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
@@ -25,6 +26,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
