3cd12b33f8a5747673c6dca32a71751c09c96cd5
nikhil <nikhilmahato3337@gmail.com>
2024-01-05 22:24:28 +0530

    concurrently and axios

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const data = await axios.get("/api/v1/test");
+console.log(data);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
