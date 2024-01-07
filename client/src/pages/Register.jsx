924ce98e5aa2e9179f9177a027e31098d378d830
nikhil <nikhilmahato3337@gmail.com>
2024-01-07 20:28:46 +0530

    use navigation

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -19,6 +19,8 @@ export const action = async ({ request }) => {
 };
 
 const Register = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -34,8 +36,8 @@ const Register = () => {
         <FormRow type="text" name="location" defaultValue="earth" />
         <FormRow type="email" name="email" defaultValue="nik@gmail.com" />
         <FormRow type="password" name="password" defaultValue="secret123" />
-        <button type="submit" className="btn btn-block">
-          submit
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? "submitting..." : "submit"}
         </button>
         <p>
           Already a member?
