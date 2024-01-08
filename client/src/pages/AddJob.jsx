f690146cb2cbb2f41984e8f819efe7f3428f8291
nikhil <nikhilmahato3337@gmail.com>
2024-01-08 23:39:25 +0530

    add job setup

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Form, useNavigation, useOutletContext } from "react-router-dom";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import FormRow from "../components/FormRow";
 
 const AddJob = () => {
-  return <h2>AddJob</h2>;
+  const { user } = useOutletContext();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  return (
+    <Wrapper>
+      <Form method="post" className="form">
+        <h4 className="form-title">addjob </h4>
+        <div className="form-center">
+          <FormRow type="text" name="position" />
+          <FormRow type="text" name="company" />
+          <FormRow
+            type="text"
+            labelText="job location"
+            name="jobLocation"
+            defaultValue={user.location}
+          />
+          <button
+            type="submit"
+            className="btn btn-block form-btn"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "submitting" : "submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default AddJob;
