5843ba30d8cb98e94b42029e89712af45a657831
nikhil <nikhilmahato3337@gmail.com>
2024-01-15 23:04:53 +0530

    job component

--- client/src/components/JobContainer.jsx
+++ client/src/components/JobContainer.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import Job from "./Job";
+import Wrapper from "../assets/wrappers/JobsContainer";
+import { useAllJobsContext } from "../pages/AllJobs";
 
 const JobContainer = () => {
-  return <div>JobContainer</div>;
+  const { data } = useAllJobsContext();
+  const { jobs } = data;
+  console.log(jobs);
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 
 export default JobContainer;
