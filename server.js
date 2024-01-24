8123e46007fc97a1e07de5035b3bcba81549238e
nikhil <nikhilmahato3337@gmail.com>
2024-01-24 14:36:37 +0530

    profile page

--- server.js
+++ server.js
@@ -11,13 +11,20 @@ import jobRouter from "./routes/jobRouter.js";
 import authRouter from "./routes/authRouter.js";
 import userRouter from "./routes/userRouter.js";
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
 import mongoose from "mongoose";
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import { authenticateUser } from "./middleware/authMiddleware.js";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+app.use(express.static(path.resolve(__dirname, "./public")));
 app.use(cookieParser());
 app.use(express.json());
 
